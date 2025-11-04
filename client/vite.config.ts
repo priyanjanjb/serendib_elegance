d37eff93add5cf908a410801eff5f1c30fcb05f6
priyanjan <priyanjanjb@gmail.com>
2025-11-04 11:18:29 +0530

    refactor: Update chunk size warning limit and improve manual chunking in Vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,19 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
-    // Increase chunk size warning limit (optional)
-    chunkSizeWarningLimit: 1000, // in kB, e.g., 1 MB
-
+    chunkSizeWarningLimit: 3000, // 3 MB
     rollupOptions: {
       output: {
-        // Manual chunks to split vendor libraries from app code
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            return 'vendor' // all dependencies in node_modules go to vendor.js
+            if (id.includes('react') || id.includes('react-dom')) return 'react-vendor';
+            return 'vendor';
           }
         },
       },
