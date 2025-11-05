bf1e7df066424b6319cc0e4b6a0f97a63e2a97a0
priyanjan <priyanjanjb@gmail.com>
2025-11-05 06:53:08 +0530

    feat: Enhance overview section with background image and updated content

--- client/src/components/overView/overView.tsx
+++ client/src/components/overView/overView.tsx
@@ -3,9 +3,8 @@ function overView() {
   return (
     <div className='overview'>
       <h1 className='overviewheding'>Overview</h1>
-      <p className='overviewpara'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's <br />
-        standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make <br />
-        a type specimen book. It has survived not only five centuries, but also the leap into electronic</p>
+      <p className='overviewpara'>
+        Serendib Elegance offers a unique blend of adventure and cultural experiences, showcasing the island's breathtaking landscapes and rich heritage. With a dedicated team of our travel experts, the agency curates personalized itineraries that cater to diverse interests, from serene beach getaways to thrilling wildlife safaris. Committed to exceptional service, the agency ensures that every journey is memorable, providing travelers with insights into local traditions and hidden gems. Whether exploring ancient temples or indulging in the vibrant culinary scene, clients can expect an unforgettable exploration of Sri Lanka's enchanting beauty.</p>
     </div>
   )
 }
